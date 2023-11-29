0d8639d5d8cc4ea131e44283b2c38cc0c31ffef2
Vera <vera@Veras-MacBook-Pro.local>
2023-11-29 15:50:42 -0500

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
+import { useKeepAwake } from 'expo-keep-awake';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  useKeepAwake();
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world!</Text>
       <StatusBar style="auto" />
     </View>
   );
